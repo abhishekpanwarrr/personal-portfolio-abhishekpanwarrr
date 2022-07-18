8e7c1f1e4e67174f993a048ee4fca027c7008970
Abhishek <Abhishekdevpanwar@gmail.com>
2022-07-18 20:36:41 +0530

    Updated site

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,25 +15,13 @@ const Navbar = ({ theme, setTheme }) => {
         <a href="#footer" className="btn">
           <span className="btn-text">Contact</span>
         </a>
-        {/* <Link to="/blog" className="btn">
-          <span className="btn-text">Blogs</span>
-        </Link> */}
-        {/* <Link to="/blog" className="btn">
-          <span className="btn-text">Blogs</span>
-        </Link> */}
       </div>
-      {/* <div className="toggle-container"> */}
       <div
         className={`dialog-button ${theme ? "" : "disabled"}`}
         onClick={setTheme}
       >
         {theme ? <BsMoon size="20px" /> : <BsSun size="20px" />}
       </div>
-      {/* </div> */}
-      {/* <div className="tooltip"> */}
-      {/* <ToggleButton theme={theme} setTheme={setTheme} /> */}
-      {/* <span className="tooltiptext">Change theme </span>
-        </div> */}
     </div>
   );
 };
