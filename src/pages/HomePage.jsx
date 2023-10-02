b555dc4f7186e65d89597c5856374d6c16b2a78f
Abhishek <40629087+abhishekpanwarrr@users.noreply.github.com>
2023-10-02 20:56:01 +0530

    added shadcn and tailwind in app

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ const Footer = React.lazy(() => import("../components/Footer/Footer"));
 const Header = React.lazy(() => import("../components/Header/Header"));
 const Projects = React.lazy(() => import("../components/Projects/Projects"));
 const Skills = React.lazy(() => import("../components/Skills/Skills"));
+const Accordion = React.lazy(() => import("../components/Accordion/AccordionDiv"));
 const HomePage = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const HomePage = () => {
       <Projects />
       <Skills />
       <Footer />
+      <Accordion />
     </>
   );
 };
