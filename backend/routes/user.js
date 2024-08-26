7cab7933322b5aebd4fd163a2601e144cc8e05a1
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2024-08-26 11:50:08 +0530

    changes

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { storeUser } from "../controllers/user.js";
-
-const router = express.Router();
-// READ
-router.post("/add", storeUser);
-
-export default router;
